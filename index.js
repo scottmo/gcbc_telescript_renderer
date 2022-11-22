d8389beecd5a6bb6275cd2a581c231bee1efb676
Scott Mo 莫少 <scottmo@users.noreply.github.com>
2022-11-21 22:19:10 -0600

    add lines to separate verses

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ input.addEventListener("change",  async (e) => {
         text = text.replace(/\n\s*(.+?)：/g, "\n\n__$1__：");
 
         inserts.forEach(({key, value}) => {
-            text = text.replace(key, key + "\n\n" + value.trim().split("\n").map(s => "> " + s).join("\n\n"));
+            text = text.replace(key, key + "\n\n" + value.trim().split("\n").map(s => {
+                if (s.trim() === "") {
+                    return "> --"; // separate verse
+                }
+                return "> " + s;
+            }).join("\n\n"));
         });
 
         output.innerHTML = converter.makeHtml(text);
