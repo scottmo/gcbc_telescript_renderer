5e3a0bddfee64d7764f43a0b0f2ed6dcd5098244
Scott Mo 莫少 <scottmo@users.noreply.github.com>
2022-11-21 21:50:34 -0600

    move js code to file

---
+++ index.js
@@ -0,0 +1,39 @@
+function $(sel) {
+    return document.querySelector(sel);
+}
+
+const converter = new showdown.Converter();
+const input = $("#input");
+const output = $("#output");
+
+let zoomLevel = 2;
+output.style.zoom = zoomLevel;
+$("#zoomin").addEventListener("click", () => {
+    output.style.zoom = ++zoomLevel;
+});
+$("#zoomout").addEventListener("click", () => {
+    output.style.zoom = --zoomLevel;
+});
+
+input.addEventListener("change",  async (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = async (e) => { 
+        let text = (e.target.result);
+
+        if (text.includes("（")) {
+            text = text.replaceAll("（", "_（");
+        }
+        if (text.includes("）")) {
+            text = text.replaceAll("）", "）_");
+        }
+        text = text.replace(/\n\s*(.+?)：/g, "\n\n__$1__：");
+
+        inserts.forEach(({key, value}) => {
+            text = text.replace(key, key + "\n\n" + value.trim().split("\n").map(s => "> " + s).join("\n"));
+        });
+
+        output.innerHTML = converter.makeHtml(text);
+    };
+    reader.readAsText(e.target.files[0])
+});
